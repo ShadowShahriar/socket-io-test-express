e543a912fb018af0d8e607ce689ad571d73008a4
Shahriar <shadowshahriar.dev@gmail.com>
2021-03-26 00:23:01 +0600

    Add cors options

--- server.js
+++ server.js
@@ -2,30 +2,23 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const cors = require('cors')
-const io = require('socket.io')(server)
+const options = {
+	cors: true
+}
+const io = require('socket.io')(server, options)
 const port = 3000
 
 app.set('port', process.env.PORT || port)
 app.set('view engine', 'ejs')
 
-// app.use(cors({
-//     origin: 'https://localhost:3000',
-//     methods: 'GET,HEAD,POST',
-//     credentials: true
-// }))
-
 app.use(express.static('public'))
 
 app.get('/', (request, response) => {
 	let newRoom = "test-page"
-	response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "X-Requested-With");
 	response.redirect('/' + newRoom)
 })
 
 app.get('/:session', (request, response) => {
-	response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "X-Requested-With");
 	response.render('index', {
 		ctrlSessionID: request.params.session
 	})
